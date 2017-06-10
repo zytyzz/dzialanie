4e31eeda77612f3a71023dd18e7379e0798a06ef
zytyzz <gsyta1@wp.pl>
2017-06-10 11:18:21 +0100

    add comments & alert

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,11 @@ function dzialanie(){
   var napis = 0;
 
   if (dzialanie > 0) {
-	console.log("Liczba jest większa od 0");  
+	console.log("Liczba jest większa od 0"); //wyswietli "Liczba jest większa od 0"
   } else if (dzialanie < 0) {
-	console.log("Liczba jest mniejsza od 0");
+	console.log("Liczba jest mniejsza od 0"); //wyswietli "Liczba jest mniejsza od 0"
   } else {
-	console.log('Liczba równa się 0');
+	console.log("Liczba równa się 0"); //wyswietli "Liczba równa się 0"
+	alert("Liczba równa sie 0"); //wyswietli alert "Liczba równa sie 0"
   };
 }
